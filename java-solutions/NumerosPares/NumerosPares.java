083a535fc45958d48cfd9c667503fdc91ba6e647
Fausto Neris <fausto2102@hotmail.com>
2022-11-27 21:25:24 -0300

    adicionando novos exercicios - 27/11

---
+++ java-solutions/NumerosPares/NumerosPares.java
@@ -0,0 +1,13 @@
+package NumerosPares;
+
+public class NumerosPares {
+    public static void main(String[] args) {
+        int number = 100; 
+
+        for(int i = 1; i <= number; i++) { 
+            if (i % 2 == 0) { 
+                System.out.println(i);
+            }
+        }
+    }
+}
